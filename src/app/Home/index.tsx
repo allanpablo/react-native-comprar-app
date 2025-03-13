77dae0afc996905a63842e0a7638c4b1c3d7697a
arthur <arthur.rios007@gmail.com>
2025-03-13 18:06:27 -0300

    Componentes - Content

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -9,8 +9,13 @@ export function Home() {
     <View style={styles.container}>
       <Image source={require('@/assets/logo.png')} style={styles.logo} />
 
-      <Input placeholder="O que você precisa comprar?" />
-      <Button title="Entrar" />
+      <View style={styles.form}
+      >
+        <Input placeholder="O que você precisa comprar?" />
+        <Button title="Entrar" />
+      </View>
+
+      <View style={styles.content}></View>
     </View>
   )
 }
